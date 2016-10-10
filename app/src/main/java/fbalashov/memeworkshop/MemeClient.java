c5621b9bcdaed4ef0e237c3c1363f4a638cd2fab
Fuad Balashov <Fuad.Balashov@appian.com>
2016-10-10 16:23:20 -0400

    Initializing App

---
+++ app/src/main/java/fbalashov/memeworkshop/MemeClient.java
@@ -0,0 +1,71 @@
+package fbalashov.memeworkshop;
+
+import java.io.IOException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+
+public class MemeClient {
+
+
+  private static final String CLIENT_ID = "99a8cba633b8ee3";
+  private static final String IMGUR_URL = "https://api.imgur.com/3/memegen/defaults";
+
+  private final OkHttpClient client;
+
+  public MemeClient() {
+    this.client = new OkHttpClient();
+  }
+
+  /**
+   * Fetches a meme from imgur and displays it within the supplied image view.
+   *
+   * @param imageId   the number of the meme
+   * @param activity  the activity that will display the meme
+   * @param imageView the image view that will hold the meme
+   */
+  public void displayMeme(final String imageId, final Activity activity, final ImageView imageView) {
+    Request getRequest = new Request.Builder().build();
+
+    client.newCall(getRequest).enqueue(new Callback() {
+      Handler handler = new Handler(activity.getMainLooper());
+
+      @Override
+      public void onFailure(Call call, IOException e) {
+        e.printStackTrace();
+
+        handler.post(new Runnable() {
+          @Override
+          public void run() {
+            Toast.makeText(activity, "Could not get meme", Toast.LENGTH_SHORT).show();
+          }
+        });
+      }
+
+      @Override
+      public void onResponse(Call call, Response response) throws IOException {
+
+      }
+    });
+  }
+
+  private String getMemeLink(String imageId, Response response) throws JSONException, IOException {
+    JSONObject object = new JSONObject(response.body().string());
+    JSONArray array = object.getJSONArray("data");
+    JSONObject memeData = array.getJSONObject(Integer.parseInt(imageId));
+    return memeData.getString("link");
+  }
+}
